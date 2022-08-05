b7caa9f21eb3049e72cc22c07d5212eef0412b6f
Khurshid <100370321+sxnpaii@users.noreply.github.com>
2022-08-05 12:58:41 +0500

    Update slick.js

--- slick.js
+++ slick.js
@@ -47,9 +47,6 @@ $('.responsive').slick({
         slidesToScroll: 1
       }
     }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
   ]
 });
 
